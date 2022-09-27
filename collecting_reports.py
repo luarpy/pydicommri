edd4889eb859d8c252034b20e944652ee55e9816
luarpy <biurrun.121721@e.unavarra.es>
2022-09-27 15:55:44 +0200

    Genera un dicionario en el que se han recogido todas las variables de interes

--- collecting_reports.py
+++ collecting_reports.py
@@ -11,15 +11,17 @@ import warnings
 from pydicom.fileset import FileSet
 from pydicom.data import get_testdata_file
 import pydicom
+import pandas as pd
 
-report_regex_name = re.compile('vibe_q-dixon_tra_bh_higado_[0-9][0-9]_Report', re.IGNORECASE)
+report_regex_name = re.compile('^vibe_q-dixon_tra_bh_higado_[0-9][0-9]_Report$', re.IGNORECASE)
 base_path = ''
 modality = 'MR'
+instance_number = 1
 out_excel_path = ''
 ff_index = ['User_ID','FF ROI','std ROI','FF Vol','std Vol','filename','Comments']
 warnings.filterwarnings("error")    
 
-def get_report(ds_overlay, output_dict):
+def get_report(ds_overlay):
     anno_seq = ds_overlay['GraphicAnnotationSequence']
     prev_str = ''
     k_section = 1
@@ -32,44 +34,43 @@ def get_report(ds_overlay, output_dict):
             pass # Otro tipo de anotaciones sin aparente interés
         else:
             aux_str = anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value
-            if aux_str=='ROI' and k_section==1:
+            if aux_str == 'ROI' and k_section == 1:
                 anno_i = next(my_iter)
-                output_dict['Area_ROI']=int(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value)
+                output_dict['Area_ROI'] = int(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value)
                 anno_i = next(my_iter)
-                output_dict['Mean_Fit_Error_ROI']=float(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value[0:-1])
-                anno_i = next(my_iter)
-                anno_i = next(my_iter)
-                output_dict['Vol_liver']=int(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value)
-                anno_i = next(my_iter)
-                output_dict['Mean_Fit_Error_Vol']=float(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value[0:-1])
-                k_section = k_section+1
-            elif aux_str=='ROI' and k_section==2:
-                k_section = k_section+1
-            elif aux_str=='ROI' and k_section==3:
-                anno_i = next(my_iter)
-                output_dict['FF_ROI']=float(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value[0:-1])
-                anno_i = next(my_iter)
-                output_dict['std_FF_ROI']=float(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value[0:-1])
+                output_dict['Mean_Fit_Error_ROI'] = float(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value[0:-1])
                 anno_i = next(my_iter)
                 anno_i = next(my_iter)
-                output_dict['FF_Vol']=float(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value[0:-1])
+                output_dict['Vol_liver'] = int(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value)
                 anno_i = next(my_iter)
-                output_dict['std_FF_Vol']=float(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value[0:-1])
-                k_section = k_section+1
-            elif aux_str=='ROI' and k_section==4:
-                k_section = k_section+1
-            elif aux_str=='ROI' and k_section==5:
+                output_dict['Mean_Fit_Error_Vol'] = float(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value[0:-1])
+                k_section -= -1
+            elif aux_str == 'ROI' and k_section == 2:
+                k_section -= -1
+            elif aux_str == 'ROI' and k_section == 3:
                 anno_i = next(my_iter)
-                output_dict['R2*_ROI']=float(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value[0:-4])
+                output_dict['FF_ROI'] = float(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value[0:-1])
                 anno_i = next(my_iter)
-                output_dict['std_R2*_ROI']=float(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value[0:-4])
+                output_dict['std_FF_ROI'] = float(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value[0:-1])
                 anno_i = next(my_iter)
                 anno_i = next(my_iter)
-                output_dict['R2*_Vol_px']=float(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value[0:-4])
+                output_dict['FF_Vol'] = float(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value[0:-1])
                 anno_i = next(my_iter)
-                output_dict['std_R2*_Vol']=float(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value[0:-4])
-                k_section = k_section+1
-            #print(k_section,aux_str)
+                output_dict['std_FF_Vol'] = float(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value[0:-1])
+                k_section -= -1
+            elif aux_str == 'ROI' and k_section == 4:
+                k_section -= -1
+            elif aux_str == 'ROI' and k_section == 5:
+                anno_i = next(my_iter)
+                output_dict['R2*_ROI'] = float(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value[0:-4])
+                anno_i = next(my_iter)
+                output_dict['std_R2*_ROI'] = float(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value[0:-4])
+                anno_i = next(my_iter)
+                anno_i = next(my_iter)
+                output_dict['R2*_Vol_px'] = float(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value[0:-4])
+                anno_i = next(my_iter)
+                output_dict['std_R2*_Vol'] = float(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value[0:-4])
+                k_section -= -1
     return output_dict
 
 
@@ -79,13 +80,12 @@ def reading_dcm_files(input_folder):
     num_files = len(files)
     k = 0
     report_found = False
+    data = {}
     output_dict = {}
-    output_dict['FF_ROI'] = \
-        output_dict['std_FF_ROI'] = \
-        output_dict['FF_Vol'] = \
-        output_dict['std_FF_Vol'] = \
+    output_dict['ID'] = \
         output_dict['Filename'] = \
         output_dict['SeriesDescription'] = \
+        output_dict['Modality'] = \
         output_dict['Comments'] = ''
 
     for f_i in files:
@@ -98,31 +98,34 @@ def reading_dcm_files(input_folder):
             output_dict['Comments'] = 'Not a Dicom file'
             pass
         try: 
-            dicom_data = pydicom.dcmread(file_i, force=True) #  If False (default), raises an InvalidDicomError if the file is missing the File Meta Information header. Set to True to force reading even if no File Meta Information header is found.
+            dicom_data = pydicom.dcmread(file_i) 
+        except RuntimeWarning:
+            output_dict['Comments'] = 'End of file reached before delimiter'
+            dicom_data = pydicom.dcmread(file_i, force=True)
             
-            if (report_regex_name.match(dicom_data.SeriesDescription)
-                    and dicom_data.Modality == modality
-                    and dicom_data.InstanceNumber == 1): 
-                print('\tReport found: ' + dicom_data.SeriesDescription, end = '', flush=True)
-                output_dict['SeriesDescription'] = dicom_data.SeriesDescription 
+        if (report_regex_name.match(dicom_data.SeriesDescription) and dicom_data.InstanceNumber == instance_number): 
+            print('\tReport found: ' + dicom_data.SeriesDescription, end = '', flush=True)
+            output_dict['SeriesDescription'] = dicom_data.SeriesDescription 
+            output_dict['Modality'] = dicom_data.Modality
+            report_data = {}
             try:
-                output_dict = get_report(dicom_data, output_dict)
+                report_data = get_report(dicom_data)
                 output_dict['Comments'] = 'No comments' 
             except KeyError:
                 output_dict['Comments'] = 'Keyword error. Could not read values'
-
-        except RuntimeWarning:
-            output_dict['Comments'] = 'End of file reached before delimiter'
-
-        except IOError:
-            output_dict['Comments'] =  'Could not read Dicom file'
-        except Exception:
-            # traceback.print_exc()
-            output_dict['Comments'] = 'An error ocurred'
-    return output_dict
+            finally:
+                print(report_data)
+                output_dict.update(report_data)
+                print(output_dict)
+        # print(output_dict)
+        data[f_i] = {}
+        data[f_i].update(output_dict)
+        output_dict.clear()
+    return data
 
 def exit_handler(signum, frame):
-    print('\nExiting...')
+    print('\nUser stopped job...')
+    user_folder_scan.close()
     exit(2)
 
 def main():
@@ -153,28 +156,20 @@ def main():
         sys.stderr.write(base_path + " is not a directory")
         exit(1)
     cwd = os.getcwd()
+    global user_folder_scan
     user_folder_scan = os.scandir(cwd)
 
+    reports = {}
+    data = {}
     for user_i in user_folder_scan: 
         if not user_i.is_dir():
             continue
-        
         print(' ',end='\r', flush=True)
-        output_dict = reading_dcm_files(user_i.name + os.sep)
-        # TODO: add other fields expanding the dictionary
-        #         ff_i = [folder_i.name]
-        #         # ff_index = ['User_ID','FF ROI','std ROI','FF Vol','std Vol','filename','Comments']
-        #         ff_i.extend([output_dict['FF_ROI'],output_dict['std_FF_ROI'],
-        #             output_dict['FF_Vol'],output_dict['std_FF_Vol'],
-        #             output_dict['Filename'],output_dict['Comments']])
-
-        #         print(ff_i)
-        # ff_i = pd.DataFrame(ff_i,index=ff_index)
-        # df_ff = pd.concat([df_ff,ff_i],axis=1)
-
-    # df_ff = df_ff.transpose()
-    # df_ff.to_excel(writer,index=False,sheet_name=sheet_name)
-    # writer.save()
+        reports[user_i.name] = {}
+        reports[user_i.name] = reading_dcm_files(user_i.name + os.sep)
+        break
+    print(reports)
+    user_folder_scan.close()
 
                 
 
