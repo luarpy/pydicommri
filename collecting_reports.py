3f4b2241711d8e5c36456edfafdedb3e282f152a
luarpy <biurrun.121721@e.unavarra.es>
2022-09-27 12:24:33 +0200

    Conseguido recuperar la información de los reportes DICOM
    Las excepciones añadidas permiten controlar el flujo de datos y errores
    que aparecen al leer un archivo DICOM. Ahora se pueden obtener
    fácilmente las partes a través del diccionario python que se genera de
    leer el archivo DICOM

--- collecting_reports.py
+++ collecting_reports.py
@@ -1,30 +1,25 @@
-#!/bin/python3
+#!/bin/env python
 
-from ast import AsyncFunctionDef
-from email.mime import base
-from pydicom.fileset import FileSet
-from pydicom.data import get_testdata_file
-from os.path import expanduser
 import os
 import sys
-import pandas as pd
+import re # for regex expresions
+import argparse
+import signal
+import traceback
+import warnings
+
+from pydicom.fileset import FileSet
+from pydicom.data import get_testdata_file
 import pydicom
 
-
+report_regex_name = re.compile('vibe_q-dixon_tra_bh_higado_[0-9][0-9]_Report', re.IGNORECASE)
 base_path = ''
-# Path to excel output file
+modality = 'MR'
 out_excel_path = ''
-excel_file_name = 'FF_Siemens_Liver_Extreme_post.xlsx'
-sheet_name = 'FF Siemens Liver Extreme_post'
 ff_index = ['User_ID','FF ROI','std ROI','FF Vol','std Vol','filename','Comments']
+warnings.filterwarnings("error")    
 
-#####################################################################################################
-#####################################################################################################
-#####################################################################################################
-#####################################################################################################
-
-def reading_report(ds_overlay,output_dict):
-          
+def get_report(ds_overlay, output_dict):
     anno_seq = ds_overlay['GraphicAnnotationSequence']
     prev_str = ''
     k_section = 1
@@ -37,7 +32,6 @@ def reading_report(ds_overlay,output_dict):
             pass # Otro tipo de anotaciones sin aparente interés
         else:
             aux_str = anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value
-            #print(aux_str)
             if aux_str=='ROI' and k_section==1:
                 anno_i = next(my_iter)
                 output_dict['Area_ROI']=int(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value)
@@ -76,85 +70,113 @@ def reading_report(ds_overlay,output_dict):
                 output_dict['std_R2*_Vol']=float(anno_i['TextObjectSequence'][0]['UnformattedTextValue'].value[0:-4])
                 k_section = k_section+1
             #print(k_section,aux_str)
-    print(output_dict)
     return output_dict
 
 
 def reading_dcm_files(input_folder):
+    print("\n\tReading from " + input_folder)
     files = [f for f in os.listdir(input_folder) if os.path.isfile(os.path.join(input_folder, f))]
     num_files = len(files)
-    report_foundQ = False
-    k=0
+    k = 0
+    report_found = False
     output_dict = {}
-    output_dict['FF_ROI']=output_dict['std_FF_ROI']= \
-            output_dict['FF_Vol']=output_dict['std_FF_Vol']= \
-            output_dict['Filename']=output_dict['Comments']=''
+    output_dict['FF_ROI'] = \
+        output_dict['std_FF_ROI'] = \
+        output_dict['FF_Vol'] = \
+        output_dict['std_FF_Vol'] = \
+        output_dict['Filename'] = \
+        output_dict['SeriesDescription'] = \
+        output_dict['Comments'] = ''
+
     for f_i in files:
-        k=k+1
-        file_i = input_folder+f_i
+        k -= -1
+        file_i = input_folder + f_i
         print(' ',end='\r', flush=True)
-        print('     %d of %d' %(k,num_files),end='',flush=True)
+        print('\t%d of %d' % (k, num_files), end = '', flush = True)
+        output_dict['Filename'] = file_i
+        if not pydicom.misc.is_dicom(file_i):
+            output_dict['Comments'] = 'Not a Dicom file'
+            pass
         try: 
-            ds = pydicom.dcmread(file_i) 
-             
-            # if ds.SeriesDescription=='t1_vibe_e-dixon_tra_bh_pre_higado_seg_Report' and \
-            if ds.SeriesDescription=='vibe_q-dixon_tra_bh_higado_Report' and \
-                ds.Modality=='PR' and \
-                ds.InstanceNumber==1: 
-                print('         Report found: '+ f_i)
-                if report_foundQ:
-                    output_dict['Comments'] = output_dict['Comments']+' Multiple reports found '
-                report_foundQ = True
-                output_dict = reading_report(ds, output_dict)
-                output_dict['Filename'] = f_i
-                output_dict['Comments'] = 'No comments'
-        except:
-            print('     ERROR reading files!!!!!!!!!!!')
-            output_dict['Comments'] = output_dict['Comments']+' Error reading files '
-    if not report_foundQ:
-        print('     ERROR: report not found!!!!!!!!!!!')
-        output_dict['Comments'] = output_dict['Comments']+' Report not found '
+            dicom_data = pydicom.dcmread(file_i, force=True) #  If False (default), raises an InvalidDicomError if the file is missing the File Meta Information header. Set to True to force reading even if no File Meta Information header is found.
+            
+            if (report_regex_name.match(dicom_data.SeriesDescription)
+                    and dicom_data.Modality == modality
+                    and dicom_data.InstanceNumber == 1): 
+                print('\tReport found: ' + dicom_data.SeriesDescription, end = '', flush=True)
+                output_dict['SeriesDescription'] = dicom_data.SeriesDescription 
+            try:
+                output_dict = get_report(dicom_data, output_dict)
+                output_dict['Comments'] = 'No comments' 
+            except KeyError:
+                output_dict['Comments'] = 'Keyword error. Could not read values'
+
+        except RuntimeWarning:
+            output_dict['Comments'] = 'End of file reached before delimiter'
+
+        except IOError:
+            output_dict['Comments'] =  'Could not read Dicom file'
+        except Exception:
+            # traceback.print_exc()
+            output_dict['Comments'] = 'An error ocurred'
     return output_dict
 
-def main():
-    for i, arg in enumerate(sys.argv):
-        if (arg == '-i'):
-            base_path = sys.argv[i+1]
-        elif(arg == '-o'):
-            output_path = sys.argv[i+1]
+def exit_handler(signum, frame):
+    print('\nExiting...')
+    exit(2)
 
-    os.chdir(base_path)
+def main():
+    parser = argparse.ArgumentParser(description='Collect reports from DICOMM files')
+    parser.add_argument('search_directory', help='Set dir to search recursively for users reports')
+    parser.add_argument('-o', dest='output_directory', default=os.getcwd(), help='Set output dir for loaded data. If no dir is ser, it will use current working dir')
+    parser.add_argument('-f', dest='input_file',help='Set input file to search for reports')
+    args = parser.parse_args()
+
+    if args.search_directory == '':
+        parser.print_help()
+    else: 
+        base_path = args.search_directory
+
+    if not args.output_directory == '':
+        output_path = args.output_directory
+
+    # CTRL+Z and CTRL+C
+    signal.signal(signal.SIGTSTP, exit_handler)
+    signal.signal(signal.SIGINT, exit_handler)
+
     # writer = pd.ExcelWriter(out_excel_path+excel_file_name,engine = 'xlsxwriter')
     # df_ff = pd.DataFrame(index=ff_index) 
+    
+    if (os.path.isdir(base_path)):
+        os.chdir(base_path)
+    else:
+        sys.stderr.write(base_path + " is not a directory")
+        exit(1)
+    cwd = os.getcwd()
+    user_folder_scan = os.scandir(cwd)
 
-
-    base_folder_scan = os.scandir(base_path)
-
-    # # next(base_folder_scan)
-    # # test_folders = [next(base_folder_scan),next(base_folder_scan),next(base_folder_scan)]
-    # # test_folders = [next(base_folder_scan))]
-    # # for i in range(60):
-    # #     next(base_folder_scan)
-    # # test_folders = [next(base_folder_scan)]
-
-    for folder_i in base_folder_scan: #test_folders:
+    for user_i in user_folder_scan: 
+        if not user_i.is_dir():
+            continue
+        
         print(' ',end='\r', flush=True)
-        print('Processing user: ' + folder_i.name)
-        if not folder_i.name.startswith('.') and folder_i.is_dir():
-            path_i = base_path+folder_i.name
-            ff_i = [folder_i.name]
-            output_dict = reading_dcm_files(path_i+'/')
-            # ff_index = ['User_ID','FF ROI','std ROI','FF Vol','std Vol','filename','Comments']
-            ff_i.extend([output_dict['FF_ROI'],output_dict['std_FF_ROI'],
-                output_dict['FF_Vol'],output_dict['std_FF_Vol'],
-                output_dict['Filename'],output_dict['Comments']])
+        output_dict = reading_dcm_files(user_i.name + os.sep)
+        # TODO: add other fields expanding the dictionary
+        #         ff_i = [folder_i.name]
+        #         # ff_index = ['User_ID','FF ROI','std ROI','FF Vol','std Vol','filename','Comments']
+        #         ff_i.extend([output_dict['FF_ROI'],output_dict['std_FF_ROI'],
+        #             output_dict['FF_Vol'],output_dict['std_FF_Vol'],
+        #             output_dict['Filename'],output_dict['Comments']])
 
-        ff_i = pd.DataFrame(ff_i,index=ff_index)
-        df_ff = pd.concat([df_ff,ff_i],axis=1)
+        #         print(ff_i)
+        # ff_i = pd.DataFrame(ff_i,index=ff_index)
+        # df_ff = pd.concat([df_ff,ff_i],axis=1)
 
     # df_ff = df_ff.transpose()
     # df_ff.to_excel(writer,index=False,sheet_name=sheet_name)
     # writer.save()
+
+                
 
 if __name__ == "__main__":
     main()
