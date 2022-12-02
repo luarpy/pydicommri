a9072c96639fe5796ead02df543f7cbc622766f3
luarpy <biurrun.121721@e.unavarra.es>
2022-12-02 14:29:45 +0100

    Ampliadas opciones para dmcgrep.py
    - Imprime el texto que coincide con la entrada resaltado en color
    - Opción para imprimir el nombre de los archivos únicamente
    - Opción para imprimir o no el nombre del archivo en caso de haber
      coincidencias en más de un archivo
    - En caso de que el patrón sea un DataElement del Dataset, imprime el
      valor del Dataset

--- pydicom_tools/dcmgrep.py
+++ pydicom_tools/dcmgrep.py
@@ -4,39 +4,68 @@ import os
 import re
 import argparse
 import sys
-from typing import (List, AnyStr, BinaryIO, Union)
+from typing import (List, AnyStr, BinaryIO, Union, Optional)
 from re import Pattern
 
 import pydicom
-from pydicom import Dataset
+from pydicom import (Dataset, DataElement)
 from pydicom.fileutil import PathType
 from pydicom.filebase import DicomFileLike
 
-from utils import recurse
+from utils.color import Colored, RED, VIOLET
 
-def print_result():
-    pass
+def print_result(
+    text: DataElement,
+    pattern: str,
+    *,
+    color: Optional[bool],
+    **kwargs
+) -> None:
+    if color:
+        string = str(text)
+        string_split = string.split(pattern)
+        color_string = ''
+        #TODO: hacer de manera más elegante el parseo de esto. Si pattern aparece al final del string, esta no sera añadida con color
+        for i in range(len(string_split)-1):
+            color_string += string_split[i] + Colored(pattern, RED)
+        color_string += string_split[len(string_split)-1]
+        
+        print(color_string)
+    else:
+        print(text)
+
 
 def recursive_match(
     pattern: Pattern,
     ds: Dataset,
     **kwargs
-) -> str:
+) -> List[DataElement]:
     number_match = kwargs.get("number_match")
+    files_with_matches = kwargs.get("files_with_matches")
+    element = kwargs.get("element")
     count_match = 0
+    found = False
+    result = []
+
+    #TODO: make sure it tries to search throw all the data types
     for elem in ds:
         if elem.VR == 'SQ':
             [recursive_match(pattern, item, **kwargs) for item in elem.value]
         else:
             if pattern.match(str(elem.value)):
-                print(elem.value)
+                found = True
                 count_match -= -1
+                result.append(elem)
                 if count_match == number_match:
                     break
+    return result
 
 def grep(
     file: str,
     pattern: str,
+    ignore_case: Optional[bool],
+    *,
+    file_number: Optional[int],
     **kwargs,
 ) -> None:
 
@@ -44,17 +73,42 @@ def grep(
         sys.stderr.write('dcmgrep: %s: Is not a DICOM file' % file)
         return
     __dicom_data = pydicom.dcmread(file, force=True)
-    
-    if kwargs.get("ignore_case") == True:
-        __regex = re.compile(str(pattern), re.IGNORECASE)
+    result = []
+
+    # Check if pattern is a DataElement inside the Dataset
+    #TODO: elem in ds devuelve un warning. Hacer la comprobación de manera más elegante para que no aparezca el warning
+    if pattern in __dicom_data:
+        result.append(__dicom_data[pattern])
     else:
-        __regex = re.compile(str(pattern))
+        if ignore_case == True:
+            __regex = re.compile(str(pattern), re.IGNORECASE)
+        else:
+            __regex = re.compile(str(pattern))
     
-    recursive_match(__regex, __dicom_data, **kwargs)
+        result = recursive_match(__regex, __dicom_data, **kwargs)
+
+    for r in result:
+        if kwargs.get("files_with_matches"):
+            if kwargs.get("color"):
+                print(Colored(file, VIOLET))
+            else:
+                print(file)
+        else:
+            if kwargs.get("filename"):
+                if file_number > 1:
+                    if kwargs.get("color"):
+                        print(Colored(file, VIOLET), end=": ")
+                    else:
+                        print(file, end=": ")
+            print_result(r, pattern, **kwargs)
+    
 
 def dcmgrep(
     files_path: Union[PathType, BinaryIO, DicomFileLike],
     pattern: str,
+    *,
+    verbose: Optional[bool],
+    recursive: Optional[bool],
     **kwargs
     ) -> None:
     """
@@ -62,27 +116,31 @@ def dcmgrep(
     """
     for file_path in files_path:
         if os.path.isfile(file_path):
-            grep(file_path, pattern, **kwargs)
+            grep(file_path, pattern, file_number=len(files_path),**kwargs)
+        
         else:
             if recursive:
-                more_paths = [paths + '/' + child for child in os.listdir(file_path)]
+                more_paths = [file_path + '/' + child for child in os.listdir(file_path)]
                 dcmgrep(more_paths, pattern, **kwargs)
             else:
-                sys.stdout.write('dcmgrep: %s: Is a directory\n' %path)
+                if verbose:
+                    sys.stdout.write('dcmgrep: %s: Is a directory\n' %file_path)
 
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("pattern", type=str, help="regular pattern for search")
+    parser.add_argument("pattern", type=str, help="regular pattern for search or DataElement to print.")
     parser.add_argument("files", nargs="+", type=str, help="file(s) to grep")
     parser.add_argument("--ignore-case", "-i", action=argparse.BooleanOptionalAction, help="ignore case distinctions in patterns and input data, so that characters that differ only in case match each other")
     parser.add_argument("--verbose", action=argparse.BooleanOptionalAction, help="wheter to print out progress and debug messages")
     parser.add_argument("--files-with-matches", "-l", action=argparse.BooleanOptionalAction, help="supress normal output; instead print the name of each input file")
     parser.add_argument("--recursive", "-r", action=argparse.BooleanOptionalAction, help="follow directories")
+    parser.add_argument("--color", "--colour", default=True, action=argparse.BooleanOptionalAction, help="output match strings colorized")
+    parser.add_argument("--filename", default=True, action=argparse.BooleanOptionalAction, help="supress the prefixing of file names on output")
     args = parser.parse_args().__dict__
 
-    pattern = args.pop("pattern")
     files = args.pop("files")
-
+    pattern = args.pop("pattern")
+    
     dcmgrep(files, pattern, **args)
             
 if __name__ == "__main__":
